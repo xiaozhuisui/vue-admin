6291370e60a508f47106b286cd51534d448fd939
郑圳宏 <1131177344@qq.com>
2021-05-26 14:31:54 +0800

    封装

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,10 +1,20 @@
 <!--  -->
 <template>
-  <div><h1>欢迎使用全栈学习页面 学习VUE3</h1></div>
+  <div>
+    <h1>欢迎使用全栈学习页面 学习VUE3</h1>
+  </div>
 </template>
 
 <script >
 export default {
+  name: 'welcome',
+  data: () => {
+    return {}  },
+  mounted() {
+    this.$request({
+      url: '/get/list'
+    }).then(res => console.log(res))
+  }
 }
 </script>
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -2,13 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus';
-import config from './config'
-import axios from 'axios'
 import 'element-plus/lib/theme-chalk/index.css';
+import request from './utils/request'
 const app = createApp(App)
-console.log(config)
-axios.get(config.mockApi + '/get/list').then((res) => [
-  console.log(res)
-])
+app.config.globalProperties.$request = request
 app.use(router).use(ElementPlus)
   .mount('#app')
